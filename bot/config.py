db558018fc5d42f6c7e89923d726101583270f60
Algorithm-Bots <algorithmbots.git@gmail.com>
2025-08-25 02:55:50 +0600

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -10,9 +10,9 @@ class Config(object):
     LOG_CHANNEL = get_config("LOG_CHANNEL", "Sheikhhasinarthemodhirbow")
     UPDATES_CHANNEL = get_config("UPDATES_CHANNEL", None) # Without `@` LOL
      # Get these values from my.telegram.org
-    AUTH_USERS = [8154074711]
+    AUTH_USERS = [-1002136866707 8154074711 5984303934 1852362865 5449069015 5066042764 7533047591]
 # array , simplest method was AUTH_USERS = [] ; AUTH_USERS.append(your telegram id) 🌹
-    TG_BOT_TOKEN = get_config("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = get_config("TG_BOT_TOKEN", "8439914112:AAEA7493ytK2R32y49rqjOXqMhgB6Gzch0I")
     # the download location, where the HTTP Server runs
     DOWNLOAD_LOCATION = get_config("DOWNLOAD_LOCATION", "/app/downloads")
     # Telegram maximum file upload size
