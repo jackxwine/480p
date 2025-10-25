bb406228f07f91e7b559b833b9dbf9061c8e2143
jackxwine <emaileco7@gmail.com>
2025-10-26 00:54:07 +0600

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -43,7 +43,7 @@ from bot.plugins.call_back_button_handler import button
 sudo_users = "5179011789" 
 crf.append("28")
 codec.append("libx264")
-resolution.append("854x480")
+resolution.append("1280x720")
 preset.append("veryfast")
 audio_b.append("40k")
 # 🤣
